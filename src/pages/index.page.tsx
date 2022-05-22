83a62f5f9447e2e4e91b8143e08530147e75dcef
poteboy <keitatkins4@gmail.com>
2022-05-22 13:48:46 +0900

    create image component

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import { View, Button, Text, VStack } from '@src/ui-core';
+import { View, Button, Text, VStack, Image } from '@src/ui-core';
 
 const Home = () => {
   return (
     <>
-      <View ml={4} sp={{ background: 'black' }}>
-        <Button variant="primary">
-          <Text color="white">hello</Text>
-        </Button>
-        <Text variant="heading">hello</Text>
-        <Text tag="p">hello</Text>
-        <Text tag="span">hello</Text>
-      </View>
-      <VStack tab={{ background: 'tomato' }}>
-        <Text variant="heading">hello</Text>
-        <Text tag="p">hello</Text>
-        <Text tag="span">hello</Text>
+      <VStack height="100vh" alignItems="center" justifyContent="center">
+        <></>
       </VStack>
     </>
   );
--- src/ui-core/index.ts
+++ src/ui-core/index.ts
@@ -4,3 +4,4 @@ export * from './HStack/HStack';
 export * from './VStack/VStack';
 export * from './Spacer/Spacer';
 export * from './Text/Text';
+export * from './Image/Image'
